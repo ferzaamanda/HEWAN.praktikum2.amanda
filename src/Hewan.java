a0bd7bbb42f9281ab3f73b9fe73e083baa63246e
ferzaamanda <ferzaamanda3@gmail.com>
2023-03-20 19:37:07 +0700

    Initial commit

---
+++ src/Hewan.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Hewan {
+
+    public static void main(String[] args) {
+        // Membuat objek Hewan dan menambahkan beberapa elemen
+        List<String> Hewan = new ArrayList<>();
+        Hewan.add("Sapi");
+        Hewan.add("Kelinci");
+        Hewan.add("Kambing");
+        Hewan.add("Unta");
+        Hewan.add("Domba");
+
+        // Membuat objek Delete Hewan dan menambahkan beberapa elemen
+        List<String> DeleteHewan = new ArrayList<>();
+        DeleteHewan.add("Kelinci");
+        DeleteHewan.add("Kambing");
+        DeleteHewan.add("Unta");
+
+        // Menghapus data pada objek Hewan yang sama dengan data warna pada objek Delete Hewan
+        Iterator<String> iterator = Hewan.iterator();
+        while (iterator.hasNext()) {
+            String hewan = iterator.next();
+            if (DeleteHewan.contains(hewan)) {
+                iterator.remove();
+
+                // Menampilkan elemen pada objek Hewan setelah data dihapus
+                System.out.println("Isi objek Hewan setelah penghapusan data: " + Hewan);
+            }
+        }
+    }
+}
+
